9f1a2d493359203c12fa9fcf4142d2e3f43ba8c4
svarkhinos <svitlana.arkhinos@gmail.com>
2021-05-21 10:09:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,21 +11,16 @@ const menuContainer = document.querySelector('.js-menu');
 const switcher = document.querySelector('#theme-switch-toggle');
 const body = document.querySelector('body');
 
-const dishesMarkup = createDishesMenu(menu);
+const dishesMarkup = dishCards(menu);
 menuContainer.insertAdjacentHTML('beforeend', dishesMarkup);
 switcher.addEventListener('change', onSwitcherClick);
 
 currentTheme();
-switchPosition();
-
-function createDishesMenu(menu) {
-    return dishCards(menu);
-}
 
 function onSwitcherClick(evt) {
     if (evt.currentTarget.checked) {
         body.classList.replace(Theme.LIGHT, Theme.DARK);
-        localStorage.setItem('theme', 'dark-theme');
+        localStorage.setItem('theme', Theme.DARK);
     } else {
         body.classList.replace(Theme.DARK, Theme.LIGHT);
         localStorage.setItem('theme', Theme.LIGHT);
@@ -33,13 +28,10 @@ function onSwitcherClick(evt) {
 }
 
 function currentTheme() {
-    if (localStorage.getItem('theme')) {
-        body.classList.add(localStorage.getItem('theme'));
-    } else body.classList.add(Theme.LIGHT);
-}
-
-function switchPosition() {
     if (localStorage.getItem('theme') === Theme.DARK) {
         switcher.setAttribute('checked', 'checked');
+        body.classList.add(Theme.DARK);
+    } else {
+        body.classList.add(Theme.LIGHT);
     }
 }
